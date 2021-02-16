ad80734d22c31cdc5420084e7d7927d4bb0ae975
chestercheng <chester7864@gmail.com>
2021-02-16 18:03:36 +0800

    feat(build.d): bump llvm to 9.0.1

--- build.d/llvm/build-llvm.sh
+++ build.d/llvm/build-llvm.sh
@@ -5,37 +5,38 @@
 #download $pkgloc $pkgurl 3f923154ed47128f13b08eacd207d9ee
 
 tarext=tar.xz
-version=${VERSION:-6.0.0}
+version=${VERSION:-9.0.1}
+baseurl=https://github.com/llvm/llvm-project/releases/download/llvmorg-$version
 
 llvm_full=llvm-$version.src
 llvm_loc=$YHDL/$llvm_full.$tarext
-llvm_url=http://releases.llvm.org/$version/$llvm_full.$tarext
-download $llvm_loc $llvm_url 788a11a35fa62eb008019b37187d09d2
+llvm_url=$baseurl/$llvm_full.$tarext
+download $llvm_loc $llvm_url 31eb9ce73dd2a0f8dcab8319fb03f8fc
 
-clang_full=cfe-$version.src
+clang_full=clang-$version.src
 clang_loc=$YHDL/$clang_full.$tarext
-clang_url=http://releases.llvm.org/$version/$clang_full.$tarext
-download $clang_loc $clang_url 121b3896cb0c7765d690acc5d9495d24
+clang_url=$baseurl/$clang_full.$tarext
+download $clang_loc $clang_url 13468e4a44940efef1b75e8641752f90
 
 clext_full=clang-tools-extra-$version.src
 clext_loc=$YHDL/$clext_full.$tarext
-clext_url=http://releases.llvm.org/$version/$clext_full.$tarext
-download $clext_loc $clext_url 6b1d543116dab5a3caba10091d983743
+clext_url=$baseurl/$clext_full.$tarext
+download $clext_loc $clext_url c76293870b564c6a7968622b475b7646
 
 libcxx_full=libcxx-$version.src
 libcxx_loc=$YHDL/$libcxx_full.$tarext
-libcxx_url=http://releases.llvm.org/$version/$libcxx_full.$tarext
-download $libcxx_loc $libcxx_url 4ecad7dfd8ea636205d3ffef028df73a
+libcxx_url=$baseurl/$libcxx_full.$tarext
+download $libcxx_loc $libcxx_url 689a42dbae917ed2a20b92deb4fd6de7
 
 libcxxabi_full=libcxxabi-$version.src
 libcxxabi_loc=$YHDL/$libcxxabi_full.$tarext
-libcxxabi_url=http://releases.llvm.org/$version/$libcxxabi_full.$tarext
-download $libcxxabi_loc $libcxxabi_url 9d06327892fc5d8acec4ef2e2821ab3d
+libcxxabi_url=$baseurl/$libcxxabi_full.$tarext
+download $libcxxabi_loc $libcxxabi_url 1395b30a937e28779cd9707c6357b801
 
 omp_full=openmp-$version.src
 omp_loc=$YHDL/$omp_full.$tarext
-omp_url=http://releases.llvm.org/$version/$omp_full.$tarext
-download $omp_loc $omp_url eb6b8d0318a950a8192933a3b500585d
+omp_url=$baseurl/$omp_full.$tarext
+download $omp_loc $omp_url 6eade16057edbdecb3c4eef9daa2bfcf
 
 # unpack.
 mkdir -p $YHROOT/src/$FLAVOR
